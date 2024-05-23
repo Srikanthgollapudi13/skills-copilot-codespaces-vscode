50ca62c1a19f13fec4141d155ebcc9854b9d6f12
SrikanthGollapudi <srikanth.gollapudi@aa.com>
2024-05-22 19:38:16 -0500

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,23 +1,3 @@
 //create web server
-var express = require('express');
-var app = express();
-
-//use the body parser to parse the body of the request
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//connect to the database
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/comments');
-
-//create the schema
-var Schema = mongoose.Schema;
-var commentSchema = new Schema({
-    name: String,
-    comment: String,
-    date: Date
-});
-
-//create the model
-var Comment = mongoose.model('Comment', commentSchema)
+//create a web server
+var http = require('http');
